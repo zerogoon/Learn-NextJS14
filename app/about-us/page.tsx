a32e9d741466af3aa8b62dd559a6444f66f7c553
zerogoon <kckzero@gmail.com>
2024-05-28 18:19:54 +0900

    2.9 dynamic routes

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,10 @@
-import Navigation from "../../components/navigation";
+export const metadata = {
+    title: 'About us',
+}
 
 export default function AboutUs() {
     return (
         <div>
-            <Navigation />
             <h1>About us!</h1>
         </div>
     );
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,12 @@
-import Navigation from "../components/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Not Found",
+};
 
 export default function NotFound() {
     return (
         <div>
-            <Navigation />
             <h1>Not Found!</h1>
         </div>
     );
